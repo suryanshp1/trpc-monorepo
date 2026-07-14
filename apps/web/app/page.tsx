7fd0952a0c6b5c6d5c4546a60f54a51ab788186b
suryanshp1 <suryanshp1@gmail.com>
2026-07-14 11:40:24 +0530

    feat: extend chaicode tRPC endpoint and frontend to include name and age fields

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,12 +3,12 @@
 import { trpc } from "~/trpc/client";
 
 export default function Home() {
-  const { data } = trpc.chaicode.useQuery({ email: "sp1@gmail.com" })
+  const { data } = trpc.chaicode.useQuery({ email: "sp1@gmail.com", name: "Surya", age: 27 })
   return (
     <main className="min-h-screen min-w-screen flex justify-center items-center">
       <div>
         <h1 className="text-3xl">Streamyst - Stream in Style</h1>
-        <h2>Server Message: {data?.message}</h2>
+        <h2>Server Message: {data?.message} name: {data?.name} age: {data?.age}</h2>
       </div>
     </main>
   );
--- packages/trpc/server/index.ts
+++ packages/trpc/server/index.ts
@@ -7,11 +7,15 @@ export const serverRouter = router({
   health: healthRouter,
   // auth: authRouter,
   chaicode: publicProcedure
-    .input(z.object({ email: z.email() }))
-    .output(z.object({ message: z.string() }))
+    .meta({ openapi: { method: "GET", path: "/chaicode" } })
+    .input(z.object({ email: z.email(), name: z.string(), age: z.number() }))
+    .output(z.object({ message: z.string(), email: z.email(), name: z.string(), age: z.number() }))
     .query(async ({ input }) => {
       return {
-        message: `Hello Mr. ${input.email}`
+        message: `Hello Mr. ${input.email}`,
+        email: input.email,
+        name: input.name,
+        age: input.age
       }
     }),
 });
