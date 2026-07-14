8f7a2bd715ae1d25579c1d63fbedfbc57bc9f7d6
suryanshp1 <suryanshp1@gmail.com>
2026-07-14 10:42:00 +0530

    feat: update API endpoint URL, remove dev scripts, and replace health check with chaicode query

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,12 @@
 import { api } from "~/trpc/server";
 
 export default async function Home() {
-  const { status } = await api.health.getHealth.query();
+  const { message } = await api.chaicode.query({ email: "sp1@gmail.com" })
   return (
     <main className="min-h-screen min-w-screen flex justify-center items-center">
       <div>
         <h1 className="text-3xl">Streamyst - Stream in Style</h1>
-        <h2>Server Status: {status}</h2>
+        <h2>Server Message: {message}</h2>
       </div>
     </main>
   );
--- packages/trpc/server/index.ts
+++ packages/trpc/server/index.ts
@@ -1,11 +1,19 @@
-import { router } from "./trpc";
-
+import { publicProcedure, router } from "./trpc";
+import { z } from "zod";
 import { healthRouter } from "./routes/health/route";
 import { authRouter } from "./routes/auth/route";
 
 export const serverRouter = router({
   health: healthRouter,
-  auth: authRouter,
+  // auth: authRouter,
+  chaicode: publicProcedure
+    .input(z.object({ email: z.email() }))
+    .output(z.object({ message: z.string() }))
+    .query(async ({ input }) => {
+      return {
+        message: `Hello Mr. ${input.email}`
+      }
+    }),
 });
 
 export { createContext } from "./context";
